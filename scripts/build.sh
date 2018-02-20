e19007fc6c136460e03cac930f2a7a68c0dd590f
Eric Curtin <ericcurtin17@gmail.com>
2018-02-20 16:51:39 +0000

    Build in debug mode on centos i686

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,16 @@
 
 d_exe() {
   local user=$1
+  local name=$2
   local cmd=$3
 
   docker exec -u "$user" "$name" /bin/bash -c "$cmd"
 }
 
 d_run() {
-  local img=$1
-  local cmd=$2
+  local name=$1
+  local img=$2
+  local cmd=$3
 
   local user=$(id -un)
   local gid=$(id -g)
@@ -18,9 +20,9 @@ d_run() {
   docker rm -f "$name" || true
   docker run --privileged -d -v /tmp:/tmp -v "/home/$user:/home/$user" -h\
          "$name" --name "$name" "$img" init
-  d_exe "root"\
+  d_exe "root" "$name"\
     "groupadd -g $gid $group && useradd -M -s /bin/bash -g $gid -u $UID $user"
-  d_exe "$UID" "cd $PWD && $cmd"
+  d_exe "$UID" "$name" "cd $PWD && $cmd"
   docker rm -f "$name" || true
 }
 
@@ -29,17 +31,24 @@ d_compile() {
   local cxx=$2
   local pre=$3
 
+  local platform=$(printf "$name" | sed "s/mariadb-connector-c-//g")
+
   if [ -z "$pre" ]; then
     pre="true"
   fi
 
+  if [ "$platform" != "i386el6" ]; then
+    local build_type="-DCMAKE_BUILD_TYPE=Release"
+  fi
+
   local cmd="$pre && export CC=$cc && export CXX=$cxx && rm -rf bin &&\
-    mkdir bin && cd bin && cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. &&\
+    mkdir bin && cd bin &&\
+    cmake $build_type -DCMAKE_INSTALL_PREFIX:PATH=/usr .. &&\
     make -j5 VERBOSE=1 && make package &&\
     mv mariadb-connector-c-3.0.3-*.tar.gz\
-    ../packages/mariadb-connector-c-3.0.3-$name.tar.gz"
+    ../packages/mariadb-connector-c-3.0.3-$platform.tar.gz"
 
-  d_run "$doc" "$cmd"
+  d_run "$name" "$doc" "$cmd"
   docker rm -f "$name" || true
 }
 
