c5d4cc8e1bd08762e8817f2db61ff7e3e365e925
Eric Curtin <ericcurtin17@gmail.com>
2018-02-20 11:02:19 +0000

    Add i386 docker

--- dockerfiles/docker.sh
+++ dockerfiles/docker.sh
@@ -7,17 +7,16 @@ BASE="$(basename "$0")"
 
 cd "$DIR"
 
-if [ "$1" != "build" ] && \
-   [ "$1" != "pull" ] && \
-   [ "$1" != "push" ] && \
+if [ "$1" != "build" ] && [ "$1" != "pull" ] && [ "$1" != "push" ] && \
    [ "$1" != "list" ]; then
   echo -e "Usage: $BASE arg\n" \
           "arg should be build, pull, push or list"  >&2
   exit 1
 fi
 
-for dir in */*; do
-  DOCIMG=$(printf "$dir\n" | sed 's#/##' | sed 's#^#curtine/mariadb-connector-c:#')
+for dir in */*/*; do
+  dir=$(printf "$dir\n" | sed 's#/Dockerfile##')
+  DOCIMG=$(printf "$dir\n" | sed 's#/##g' | sed 's#^#curtine/mariadb-connector-c:#')
 
   if [ "$1" == "build" ]; then
     /bin/bash -c "cd $dir && docker build --build-arg=http_proxy\
